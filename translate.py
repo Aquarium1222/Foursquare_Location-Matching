ecc37aa386597aeb7a366bccc3ac562d7509dcde
gougon <freddy2531@gmail.com>
2022-06-04 02:16:28 +0800

    Feat: Modify translate

--- translate.py
+++ translate.py
@@ -4,14 +4,27 @@ import string
 
 from googletrans import Translator
 
+translator = Translator()
+
 
 def translate_text(text, dest = 'en'):
-    translator = Translator()
+    # translator = Translator()
     result = translator.translate(text, dest).text
     return result
 
 
 result = translate_text('Al Derwaza	')
+print(result)
+result = result.lower()
+print(result)
+
+result = translate_text('屁眼，好痛。')
+print(result)
+result = result.lower()
+print(result)
+
+text = 'tattokievrg'.replace('.', '. ')
+result = translate_text(text)
 print(result)
 result = result.lower()
 print(result)
