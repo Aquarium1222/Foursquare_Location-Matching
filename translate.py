f117b46183b5cf859f2e108f0fdf9e6a21b5cff3
gougon <freddy2531@gmail.com>
2022-06-05 18:28:35 +0800

    Feat: Encode text & build dataloader

--- translate.py
+++ translate.py
@@ -1,30 +1,43 @@
-# sudo pip install googletrans==4.0.0-rc1
+# pip install pygtrans
 
-import string
-
-from googletrans import Translator
-
-translator = Translator()
+from pygtrans import Translate, Null
 
 
-def translate_text(text, dest = 'en'):
-    # translator = Translator()
-    result = translator.translate(text, dest).text
-    return result
+class Translation:
+    def __init__(self):
+        self.client = Translate()
+
+    def translate(self, source):
+        text = self.client.translate(source, target='en')
+        if isinstance(text, Null):
+            print(text.msg)
+            return source
+        elif isinstance(source, str):
+            return text.translatedText
+        return [i.translatedText for i in text]
 
 
-result = translate_text('Al Derwaza	')
-print(result)
-result = result.lower()
-print(result)
+if __name__ == '__main__':
+    trans = Translation()
+    result = trans.translate('Al Derwaza	')
+    print(result)
+    result = result.lower()
+    print(result)
 
-result = translate_text('屁眼，好痛。')
-print(result)
-result = result.lower()
-print(result)
+    result = trans.translate('屁眼，好痛。')
+    print(result)
+    result = result.lower()
+    print(result)
 
-text = 'tattokievrg'.replace('.', '. ')
-result = translate_text(text)
-print(result)
-result = result.lower()
-print(result)
+    result = trans.translate('tattokievrg.org')
+    print(result)
+    result = result.lower()
+    print(result)
+
+    result = trans.translate('やめて')
+    print(result)
+    result = result.lower()
+    print(result)
+
+    result = trans.translate(['やめて', '好屌'])
+    print(result)
